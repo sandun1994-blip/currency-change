0cc88d486306ec82d409bd0ee3b4834dcfdeb52d
Sandun Tharuka <sandun.suntws@gmail.com>
2023-11-22 15:18:58 +0530

    create home page

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -5,6 +5,45 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Hello Worlddd')));
+
+    final border = OutlineInputBorder(
+        borderSide: const BorderSide(
+            width: 2,
+            style: BorderStyle.solid),
+        borderRadius: BorderRadius.circular(5));
+
+    return  Scaffold(
+        backgroundColor: Colors.green,
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+             const Text(
+                '0',
+                style: TextStyle(
+                    fontSize: 45,
+                    fontWeight: FontWeight.w700,
+                    color: Color.fromARGB(220, 238, 8, 8)),
+              ),
+              Padding(
+                padding: const EdgeInsets.all( 8.0),
+                child: TextField(
+                  style: const TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                      hintText: 'please enter',
+                      hintStyle:const TextStyle(color: Colors.white60),
+                      prefixIcon:const Icon(Icons.monetization_on_outlined),
+                      prefixIconColor: Colors.white60,
+                      filled: true,
+                      fillColor: const Color.fromARGB(255, 199, 195, 183),
+                      focusedBorder: border,
+                      enabledBorder:border,
+                     
+                      ),
+                      keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                ),
+              ),
+            ])));
   }
 }
